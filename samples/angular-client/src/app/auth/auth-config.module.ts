6e45a47a0fd45bc9c9294421925629f3555a3bd7
Steve Riesenberg <sriesenberg@vmware.com>
2022-02-23 15:34:19 -0600

    Generated angular application with angular-auth-oidc-client

---
+++ samples/angular-client/src/app/auth/auth-config.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { AuthModule } from 'angular-auth-oidc-client';
+
+
+@NgModule({
+  imports: [AuthModule.forRoot({
+    config: {
+      authority: 'http://localhost:9000',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: 'please-enter-clientId',
+      scope: 'please-enter-scopes', // 'openid profile ' + your scopes
+      responseType: 'code',
+      silentRenew: true,
+      silentRenewUrl: window.location.origin + '/silent-renew.html',
+      renewTimeBeforeTokenExpiresInSeconds: 10,
+    }
+  })],
+  exports: [AuthModule],
+})
+export class AuthConfigModule {
+}
