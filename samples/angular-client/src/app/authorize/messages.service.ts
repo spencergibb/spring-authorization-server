f3bb87d26965a024f80f083511325f9679727f91
Steve Riesenberg <sriesenberg@vmware.com>
2022-03-08 16:03:05 -0600

    Step 7 - With Credentials

--- samples/angular-client/src/app/authorize/messages.service.ts
+++ samples/angular-client/src/app/authorize/messages.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,12 @@ export class MessagesService {
   constructor(private http: HttpClient) { }
 
   getMessages(): Observable<string[]> {
-    // TODO: Request data from backend
-    return of(['Please log in.']);
+    return this.http.get<string[]>('http://127.0.0.1:8080/messages', {
+      withCredentials: true
+    }).pipe(catchError((error) => {
+      console.error(error);
+      window.location.href = 'http://127.0.0.1:8080/authorize';
+      return of(['Please log in.']);
+    }))
   }
 }
