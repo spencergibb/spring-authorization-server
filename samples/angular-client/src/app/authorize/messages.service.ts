8ff365910a6759d2005c16fb4778944a413769b6
Steve Riesenberg <sriesenberg@vmware.com>
2022-03-08 15:31:37 -0600

    Step 4 - Authorization Header

--- samples/angular-client/src/app/authorize/messages.service.ts
+++ samples/angular-client/src/app/authorize/messages.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
@@ -10,7 +10,13 @@ export class MessagesService {
   constructor(private http: HttpClient, private oidcSecurityService: OidcSecurityService) { }
 
   getMessages(): Observable<string[]> {
-    // TODO: Request data from resource server
-    return of(['Please log in.']);
+    if (this.oidcSecurityService.isAuthenticated()) {
+      let accessToken = this.oidcSecurityService.getAccessToken();
+      return this.http.get<string[]>('http://localhost:8090/messages', {
+        headers: new HttpHeaders(`Authorization: Bearer ${accessToken}`)
+      });
+    } else {
+      return of(['Please log in.']);
+    }
   }
 }
